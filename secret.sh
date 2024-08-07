aa5ff5c8e43266d0cbbfca16a66366113f632300
InDjPERU <85323699+InDjPERU@users.noreply.github.com>
2024-08-06 20:51:38 -0500

    Update secret.sh

--- secret.sh
+++ secret.sh
@@ -2,5 +2,5 @@
 #
 # * This is where all the credentials is stored, this is used when theres no token supplied on actions.
 
-FRMENV_FBTOKEN=""
+FRMENV_FBTOKEN="EAARG5qVYavwBO1RybuUG7KICQ9fMK7yOJMqNn6zCtN6JSFFytAwYl8UPVkVTZBhDBGuT1ZABQEWxPAA8qS7kadOJthgLMIrTtXUZAh1WYzNJjKBwkvRlc8OLweGsFBhBpTG01OYngwFxfDMXFT10rkNR4N8NGgJx1dzuZA7C0OxOWPQuiYa8JARQAERZAKB0J"
 FRMENV_GIFTOKEN=""
